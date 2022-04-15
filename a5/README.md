303519e8a1ff59b5238f5a82dd2d11b40ab71e7a
lilys012 <lysianthus@naver.com>
2022-04-15 16:26:20 +0900

    220415

---
+++ a5/README.md
@@ -0,0 +1,11 @@
+# cs224n
+
+Stanford cs224n Winter 2021 Assignment 5
+
+vanilla model without pretrain accuracy: 1.4%
+
+london_baseline accuracy: 5.0%
+
+vanilla model with pretrain accuracy: 23.8%
+
+synthesizer model accuracy: 11.8%
---
+++ a5/src/london_baseline.py
@@ -0,0 +1,15 @@
+# Calculate the accuracy of a baseline that simply predicts "London" for every
+#   example in the dev set.
+# Hint: Make use of existing code.
+# Your solution here should only be a few lines.
+with open("birth_dev.tsv") as fin:
+    lines = [x.strip().split('\t') for x in fin]
+    if len(lines[0]) == 1:
+        print('No gold birth places provided; returning (0,0)')
+    true_places = [x[1] for x in lines]
+    total = len(true_places)
+    predicted_places = ['London' for i in range(total)]
+    assert total == len(predicted_places)
+    correct = len(list(filter(lambda x: x[0] == x[1],
+                              zip(true_places, predicted_places))))
+    print('Correct: {} out of {}: {}%'.format(correct, total, correct / total * 100))
